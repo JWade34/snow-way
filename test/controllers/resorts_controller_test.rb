a54be33c54676adbd56bfb54096437d5ec222282
JWade34 <justinmckelvey@users.noreply.github.com>
2026-01-09 19:33:28 -0600

    Fix test route helpers

--- test/controllers/resorts_controller_test.rb
+++ test/controllers/resorts_controller_test.rb
@@ -2,12 +2,13 @@ require "test_helper"
 
 class ResortsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get resorts_index_url
+    get resorts_url
     assert_response :success
   end
 
   test "should get show" do
-    get resorts_show_url
+    resort = Resort.first || Resort.create!(name: "Test Resort", latitude: 40.0, longitude: -105.0, state: "CO")
+    get resort_url(resort)
     assert_response :success
   end
 end
