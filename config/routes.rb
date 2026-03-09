4e20ebce87f380e96db349f2e24d21308131612f
Justin <JustinWMcKelvey@gmail.com>
2026-03-09 14:49:10 -0500

    Add refresh_forecasts route for triggering data refresh

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :resorts, only: [ :index, :show ]
   resources :weather, only: [ :index ]
 
+  # Refresh forecast data
+  post "refresh_forecasts", to: "weather#refresh"
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
