cb8028285be4ce49c302e955d0944075b2b44602
Justin <JustinWMcKelvey@gmail.com>
2026-03-09 14:51:17 -0500

    Add refresh action to weather controller for triggering forecast data refresh

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,4 +3,17 @@ class WeatherController < ApplicationController
     @resorts = Resort.by_most_snow.limit(10)
     @weather_data = WeatherData.fetch_for_resorts(@resorts)
   end
+
+  def refresh
+    Resort.find_each do |resort|
+      begin
+        SnowForecast.refresh_forecasts_for!(resort)
+      rescue => e
+        Rails.logger.error "Failed to refresh forecasts for #{resort.name}: #{e.message}"
+      end
+      sleep 0.2
+    end
+
+    redirect_to resorts_path, notice: "Forecasts refreshed successfully!"
+  end
 end
